455ca058d865423aa03648e6a696607aa70b7682
liucanpkucn <can.liu@pku.edu.cn>
2020-02-16 23:49:44 +0800

    a

--- another/create_input_files.py
+++ another/create_input_files.py
@@ -3,8 +3,8 @@ from utils import create_input_files
 if __name__ == '__main__':
     # Create input files (along with word map)
     create_input_files(dataset='coco',
-                       karpathy_json_path='/home/can.liu/caption/data/annotations/captions_train2014.json ',
-                       image_folder='/home/can.liu/caption/data/train2014',
+                       karpathy_json_path='/home/can.liu/caption/data/annotations/captions_train2014.json',
+                       image_folder='/home/can.liu/caption/data/train2014/',
                        captions_per_image=5,
                        min_word_freq=5,
                        output_folder='/home/can.liu/caption/data/',
