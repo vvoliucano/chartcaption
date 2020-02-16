f339f383a220e77cf3d728104c7dd132561bfd55
liucanpkucn <can.liu@pku.edu.cn>
2020-02-17 00:14:46 +0800

    a

--- another/create_input_files.py
+++ another/create_input_files.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     # Create input files (along with word map)
     create_input_files(dataset='coco',
                        karpathy_json_path='/home/can.liu/caption/data/karpathy/dataset_coco.json',
-                       image_folder='/home/can.liu/caption/data/train2014/',
+                       image_folder='/home/can.liu/caption/data/coco_2014/',
                        captions_per_image=5,
                        min_word_freq=5,
                        output_folder='/home/can.liu/caption/data/karpathy_output/',
