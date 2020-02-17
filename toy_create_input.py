87819b403fbc5af7bf17d51ae90d5418cece3ce8
liucanpkucn <can.liu@pku.edu.cn>
2020-02-18 00:03:28 +0800

    a

---
+++ toy_create_input.py
@@ -0,0 +1,23 @@
+from utils import create_input_files
+
+# import argparse
+# parser = argparse.ArgumentParser(description='Show, Attend, and Tell - Tutorial - Generate Caption for SVG')
+
+# parser.add_argument('--karpathy_json_path', type=str, default = '/Users/tsunmac/vis/projects/autocaption/data/template.json', help='folder with data files saved by create_input_files.py')
+# parser.add_argument('--image_folder', type=str, default = 'coco_5_cap_per_img_5_min_word_freq', help='base name shared by data files')
+# parser.add_argument('--beam_size', '-b', default=5, type=int, help='beam size for beam search')
+# parser.add_argument('--dont_smooth', dest='smooth', action='store_false', help='do not smooth alpha overlay')
+
+# args = parser.parse_args()
+
+
+
+if __name__ == '__main__':
+    # Create input files (along with word map)
+    create_input_files(dataset='flickr8k',
+                       karpathy_json_path='/home/can.liu/caption/data/try/template.json',
+                       image_folder='/home/can.liu/caption/data/try/try_image/',
+                       captions_per_image=5,
+                       min_word_freq=0,
+                       output_folder='/home/can.liu/caption/data/try/try_output',
+                       max_len=50)
---
+++ train_log.sh
@@ -0,0 +1,5 @@
+
+
+python toy_create_input.py
+
+python train.py --data_folder /Users/tsunmac/vis/projects/autocaption/data/karpathy_output --data_name flickr8k_5_cap_per_img_5_min_word_freq
