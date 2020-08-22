7dcfd1258b2c6938a798c9a99d841d661e53b98d
liucanpkucn <can.liu@pku.edu.cn>
2020-08-22 20:25:26 +0800

    a

--- data_generator/before_data/sentence_template.py
+++ data_generator/before_data/sentence_template.py
@@ -1,17 +1,26 @@
 import json
 import language_check
 
+import time
+
 # a = f'sdf{sdf}'
 
 # a
 
 
 def get_checked_sentence(text = "China's GDP increase from 2019 to 2020."):
+
+	t1 = time.time()
 	tool = language_check.LanguageTool('en-US')
+	t2 = time.time()
 	# text = u'A sentence with a error in the Hitchhiker’s Guide tot he Galaxy'
 	matches = tool.check(text)
-	print(matches)
+	# print(matches)
 	new_text = language_check.correct(text, matches)
+	t3 = time.time()
+
+	print("inter", t2 - t1)
+	print('inter', t3 - t2)
 	return new_text
 
 
