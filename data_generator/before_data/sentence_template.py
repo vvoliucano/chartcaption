09f6f0d07fa2c63af41c39826dd20d8c1881c2b3
liucanpkucn <can.liu@pku.edu.cn>
2020-08-22 21:13:16 +0800

    a

--- data_generator/before_data/sentence_template.py
+++ data_generator/before_data/sentence_template.py
@@ -2,6 +2,7 @@ import json
 import language_check
 
 import time
+import random
 
 # a = f'sdf{sdf}'
 
@@ -10,17 +11,11 @@ import time
 
 def get_checked_sentence(text = "China's GDP increase from 2019 to 2020."):
 
-	t1 = time.time()
 	tool = language_check.LanguageTool('en-US')
-	t2 = time.time()
 	# text = u'A sentence with a error in the Hitchhiker’s Guide tot he Galaxy'
 	matches = tool.check(text)
 	# print(matches)
 	new_text = language_check.correct(text, matches)
-	t3 = time.time()
-
-	print("inter", t2 - t1)
-	print('inter', t3 - t2)
 	return new_text
 
 
@@ -28,15 +23,20 @@ f = open('template.json')
 sentence_template = json.load(f)
 
 
-def get_trend_sentence(name, begin, end, begin_value, end_value):
-	sen1 = sentence_template["trend_inc"][0].format(name = "{name}", begin = begin, end = end, end_value = end_value)
-	sen2 = sen1.format(name = "this value")
+def get_trend_sentence(name, begin, end, begin_value, end_value, cap_type = "trend_inc"):
+
+
+	# sen1 = sentence_template["trend_inc"][0].format(name = "{name}", begin = begin, end = end, end_value = end_value)
+	# sen2 = sen1.format(name = "this value")
 	# print(sen1)
 	# print(sen2)
 
-	template_choice = sentence_template["trend_inc"]
+	template_choice = sentence_template[cap_type]
+	current_template = random.choice(template_choice)
 
-	return sen2
+	sentence = current_template.format(name = name, begin = begin, end = end, end_value = end_value)
+
+	return sentence
 
 
 
@@ -46,8 +46,10 @@ if __name__ == '__main__':
 	end = 2020
 	begin_value = 20
 	end_value = 200
-	sentence = get_trend_sentence("China's GDP", begin, end, begin_value, end_value)
-	print("original_sentence", sentence)
+	cap_type = "trend_inc"
+	sentence = get_trend_sentence("China's GDP", begin, end, begin_value, end_value, cap_type = cap_type)
+
+	print("Sentence: ", sentence)
 
 	new_sentence = get_checked_sentence(sentence)
 	print("new_sentence", new_sentence)
