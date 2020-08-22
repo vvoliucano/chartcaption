9f81c5cd98dcc3c2eb77e164fdb61ec3c0c1b8de
liucanpkucn <can.liu@pku.edu.cn>
2020-08-22 20:22:50 +0800

    a

--- data_generator/before_data/sentence_template.py
+++ data_generator/before_data/sentence_template.py
@@ -10,6 +10,7 @@ def get_checked_sentence(text = "China's GDP increase from 2019 to 2020."):
 	tool = language_check.LanguageTool('en-US')
 	# text = u'A sentence with a error in the Hitchhiker’s Guide tot he Galaxy'
 	matches = tool.check(text)
+	print(matches)
 	new_text = language_check.correct(text, matches)
 	return new_text
 
