a40287fcd79d7868258250d9307dcb3197a54b5e
liucanpkucn <can.liu@pku.edu.cn>
2020-08-29 16:33:35 +0800

    a

--- test_module.py
+++ test_module.py
@@ -351,7 +351,7 @@ def get_word_seq_score(seqs, rev_word_map, replace_dict, scores):
 
         current_sentence = {}
         current_sentence["sentence"] = words
-        current_sentence["score"] = float(scores[i])
+        current_sentence["score"] = float(scores[seq_index])
         sentences.append(current_sentence)
 
     print("sentences: ", sentences)
